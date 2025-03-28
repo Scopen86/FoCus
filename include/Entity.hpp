e3f08a1e26223288ba4559fa21cdc8360c4dbb51
Scopen <scopen86@gmail.com>
2025-03-28 19:33:09 +0700

    Handle player movemen

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -1,3 +1,4 @@
+//TODO: Implement destructor to free texture memory, Implement sprite sheet animation?
 #pragma once
 #include <SDL.h>
 #include "Vector2f.hpp"
@@ -15,10 +16,13 @@ class Entity {
         SDL_Texture* getTex() const { return tex; }
         Vector2f getPosition() const { return pos; }
         Vector2f getSize() const { return size; }
+        float getSpeed() const { return speed; }
+
         void move(Vector2f delta) { pos = pos + delta; }
         void setPos(Vector2f newPos) { pos = newPos; }
     private:
         Vector2f pos;
         Vector2f size;
         SDL_Texture* tex;
+        const float speed = 3.0;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,34 @@ int main(int argc, char* argv[]) {
                 gameRunning = false;
             }
         }
+
+        // Handle player movement
+        Vector2f deltaPos(0, 0);
+        const Uint8* state = SDL_GetKeyboardState(NULL);
+        if(state[SDL_SCANCODE_W] || state[SDL_SCANCODE_UP]) {
+            deltaPos.y -= player.getSpeed();
+        }
+        if(state[SDL_SCANCODE_S] || state[SDL_SCANCODE_DOWN]) {
+            deltaPos.y += player.getSpeed();
+        }
+        if(state[SDL_SCANCODE_A] || state[SDL_SCANCODE_LEFT]) {
+            deltaPos.x -= player.getSpeed();
+        }
+        if(state[SDL_SCANCODE_D] || state[SDL_SCANCODE_RIGHT]) {
+            deltaPos.x += player.getSpeed();
+        }
+        player.move(deltaPos);
+
+        // Prevent player from going off screen
+        Vector2f playerPos = player.getPosition();
+        if(playerPos.x < 0) player.setPos(Vector2f(0, playerPos.y));
+        if(playerPos.y < 0) player.setPos(Vector2f(playerPos.x, 0));
+        if(playerPos.x + player.getSize().x > SCREEN_WIDTH) {
+            player.setPos(Vector2f(SCREEN_WIDTH - player.getSize().x, playerPos.y));
+        }
+        if(playerPos.y + player.getSize().y > SCREEN_HEIGHT) {
+            player.setPos(Vector2f(playerPos.x, SCREEN_HEIGHT - player.getSize().y));
+        }
         
         window.clear();
         window.draw(background);
@@ -34,6 +62,6 @@ int main(int argc, char* argv[]) {
         window.draw(enemy);
         window.display();
     }
-    
+
     return 0;
 }
